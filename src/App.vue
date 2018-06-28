2c0156350a47f63b4367b04dcba749b91662fd22
ixjx <vissky1@163.com>
2018-06-28 14:41:39 +0800

    use JqueryUI tabs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <baidu></baidu>
-    <sina></sina>
+    <div id="tabs">
+      <ul>
+        <li><a href="#tabs-1">新浪</a></li>
+        <li><a href="#tabs-2">百度</a></li>
+      </ul>
+      <div id="tabs-1">
+        <sina></sina>
+      </div>
+      <div id="tabs-2">
+        <baidu></baidu>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +25,10 @@ export default {
     Sina
   }
 }
+
+$(function(){
+  $("#tabs").tabs();
+});
 </script>
 
 <style>
@@ -23,6 +37,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#tabs li {
+  width: 49.6%;
+}
+#tabs li a {
+  width: 100%;
+  text-align: center;
+}
+.ui-widget-content a {
+  color: #003eff
 }
 </style>
